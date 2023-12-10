bc3ab5e786a44b0f607b181da2ad0a03aaa98064
Thomas Heinen <33926466+thheinen@users.noreply.github.com>
2023-12-10 15:04:16 +0000

    Fixed file_open for now
    Signed-off-by: Thomas Heinen <33926466+thheinen@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ sudo cp tk_code/train.rb ~/.chef/gem/ruby/3.1.0/gems/kitchen-transport-train-0.1
 sudo cp -r tk_code/chef/* /opt/chef-workstation/embedded/lib/ruby/gems/3*/gems/chef-1*/
 sudo cp -r tk_code/chef-config/* /opt/chef-workstation/embedded/lib/ruby/gems/3*/gems/chef-config-*/
 sudo cp -r tk_code/mixlib/* /opt/chef-workstation/embedded/lib/ruby/gems/3*/gems/mixlib-shellout-*/
+sudo cp -r tk_code/train/* /opt/chef-workstation/embedded/lib/ruby/gems/3*/gems/train-core-*/
