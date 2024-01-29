5caac0c0ac686fdef26f1be98ba1ca5148dbe3ad
Thomas Heinen <33926466+thheinen@users.noreply.github.com>
2024-01-29 07:50:13 +0000

    Merge changes into upstream repos
    Signed-off-by: Thomas Heinen <33926466+thheinen@users.noreply.github.com>

--- tk_code/chef/lib/chef/target_io.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'target_io/dir'
-require_relative 'target_io/etc'
-require_relative 'target_io/file'
-require_relative 'target_io/fileutils'
-require_relative 'target_io/io'
-
-require_relative 'target_io/train_compat'
