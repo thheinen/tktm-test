28f44344eb388c1f57dbdf9164157eba3aa598ae
Thomas Heinen <33926466+thheinen@users.noreply.github.com>
2024-02-01 16:58:24 +0000

    Cleaup after rollup into forks
    Signed-off-by: Thomas Heinen <33926466+thheinen@users.noreply.github.com>

--- tk_code/chef/lib/chef/target_io/train_compat.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'train/dir'
-require_relative 'train/etc'
-require_relative 'train/file'
-require_relative 'train/fileutils'
-require_relative 'train/io'
-require_relative 'train/shadow'
