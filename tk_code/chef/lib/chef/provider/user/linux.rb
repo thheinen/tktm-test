3e5d28e3fc5f7455103ed1d68dd2d5a007026af6
Thomas Heinen <33926466+thheinen@users.noreply.github.com>
2024-02-01 13:45:10 +0000

    Support for users and Shadow
    Signed-off-by: Thomas Heinen <33926466+thheinen@users.noreply.github.com>

--- tk_code/chef/lib/chef/provider/user/linux.rb
+++ tk_code/chef/lib/chef/provider/user/linux.rb
@@ -28,6 +28,11 @@ class Chef
           load_shadow_options
         end
 
+        def supports_ruby_shadow?
+          # For target mode, ruby-shadow is redirected to a file-based implementation
+          true if !ChefConfig::Config.target_mode?
+        end
+
         def compare_user
           user_changed = super
 
---
+++ tk_code/chef/lib/chef/target_io/train_compat.rb
@@ -0,0 +1,6 @@
+require_relative 'train/dir'
+require_relative 'train/etc'
+require_relative 'train/file'
+require_relative 'train/fileutils'
+require_relative 'train/io'
+require_relative 'train/shadow'
