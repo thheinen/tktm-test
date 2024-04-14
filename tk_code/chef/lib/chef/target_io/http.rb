ea36bd0c7b23a394ae7209fa755f6dfc0419c3b1
Thomas Heinen <33926466+thheinen@users.noreply.github.com>
2024-04-14 13:53:55 +0000

    Add http_request and apt_repository with PPA urls
    Signed-off-by: Thomas Heinen <33926466+thheinen@users.noreply.github.com>

--- tk_code/chef/lib/chef/target_io/http.rb
+++ tk_code/chef/lib/chef/target_io/http.rb
@@ -1,44 +1,19 @@
-# require_relative "../mixin/which"
+require 'forwardable'
+
+require_relative "../http/simple"
+require_relative "train/http"
 
 module TargetIO
   class HTTP
-    SUPPORTED_COMMANDS = %w[curl wget]
+    extend Forwardable
+    def_delegators :@http_class, :get, :head, :patch, :post, :put, :delete
 
-    def initialize(_); end
-
-    def method_missing(verb, url, headers = {})
-      cmd = nil
-      SUPPORTED_COMMANDS.each do |name|
-        executable = which(name).chop
-        next unless executable
-
-        cmd = self.send(name.to_sym, executable, verb.to_s.upcase, url, headers)
-        break
+    def initialize(url)
+      if ::ChefConfig::Config.target_mode?
+        @http_class = TargetIO::TrainCompat::HTTP.new(url)
+      else
+        @http_class = Chef::HTTP::Simple.new(url)
       end
-
-      raise "Target needs one of #{SUPPORTED_COMMANDS.join('/')} for HTTP requests to work" unless cmd
-
-      connection = Chef.run_context&.transport_connection
-      connection.run_command(cmd).stdout
-    end
-
-    def curl(cmd, verb, url, headers)
-      cmd += headers.map { |name, value| " --header '#{name}: #{value}'"}.join
-      cmd += " --request #{verb} "
-      cmd += url
-    end
-
-    def wget(cmd, verb, url, headers)
-      cmd += headers.map { |name, value| " --header '#{name}: #{value}'"}.join
-      cmd += " --method #{verb}"
-      cmd += " --output-document=- "
-      cmd += url
-    end
-
-    # extend Chef::Mixin::Which
-    def which(cmd)
-      connection = Chef.run_context&.transport_connection
-      connection.run_command("which #{cmd}").stdout
     end
   end
 end
